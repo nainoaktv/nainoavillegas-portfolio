7f1f46a33538dcf7ef6bc63e2dd2cf4f9f1b4c26
nainoktv <nainoaktvillegas@gmail.com>
2022-12-26 20:22:22 -0800

    chore: navbar resizing and styling initial commit

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
-      <ul>
+      <ul className="app__navbar-links">
         {['home', 'about', 'work', 'skills', 'contatct'].map((item) => (
-          <li key={`link-${item}`}>
+          <li className="app__flex p-text" key={`link-${item}`}>
             <div/>
             <a href={`#${item}`}>{item}</a>
           </li>
