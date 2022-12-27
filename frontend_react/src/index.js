61705a56a64feffdedea9bbd21575577ef0d5298
nainoktv <nainoaktvillegas@gmail.com>
2022-12-26 20:02:35 -0800

    chore: fix render error within index, now using react 18 instead of 17

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import './index.css';
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+reportWebVitals();
