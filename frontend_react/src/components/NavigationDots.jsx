a949727686533db3f306ee2ed478ceedc701fe5a
nainoktv <nainoaktvillegas@gmail.com>
2022-12-29 19:45:35 -0800

    feat: render social media logos into header

---
+++ frontend_react/src/components/NavigationDots.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+
+      {['home', 'about', 'work', 'skills', 'testimonials', 'contatct'].map((item, idx) => (
+       <a
+         href={`#${item}`} 
+         key={item + idx}
+         className="app__navigation-dot"
+         style={active === item ? { backgroundColor: '#313BAC' } : { }}
+        />
+      ))}
+
+    </div>
+  )
+};
+
+export default NavigationDots;
