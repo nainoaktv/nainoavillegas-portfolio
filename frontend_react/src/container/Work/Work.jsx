f193ce3130a9188ed86ac951eacb435c7290fd00
nainoktv <nainoaktvillegas@gmail.com>
2022-12-31 01:23:35 -0800

    feat: initial commit for porfolio section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+
+  const handleWorkFilter = () => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text"><span>My </span>Portfolio</h2>
+
+      <div className="app__work-filter">
+        {['Web App', 'React JS', 'JavaScript', 'All'].map((item,index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}          
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+    </>
   );
 }
 
