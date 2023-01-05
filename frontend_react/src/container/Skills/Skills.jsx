ef586014b9a2c8ede20b9ff5bb56c8e521c572a7
nainoktv <nainoaktvillegas@gmail.com>
2023-01-04 18:45:57 -0800

    feat: render experience and reposition

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import ReactToolTip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -16,6 +15,7 @@ const Skills = () => {
 
     client.fetch(query)
     .then((data) => {
+      console.log(data);
       setExperience(data);
     })
     client.fetch(skillsQuery)
@@ -43,10 +43,39 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-
+        <motion.div className="app__skills-exp">
+          {experience.map((experience) => (
+              <motion.div
+                className="app__skills-exp-item"
+                key={experience.year}
+              >
+                <div className="app__skills-exp-year">
+                  <p className="bold-text">{experience.year}</p>
+                </div>
+                <motion.div className="app__skills-exp-works">
+                  {experience.works.map((work) => (
+                    <>
+                      <motion.div
+                        whileInView={{ opacity: [0, 1]} }
+                        transition={{ duration: 0.5 }}          
+                        className="app__skills-exp-work"
+                        data-tip
+                        data-for={work.name}
+                        key={work.name}
+                      >
+                        <h4 className="bold-text">{work.name}</h4>
+                        <p className="p-text">{work.company}</p>
+                        <p className="p-text">{work.desc}</p>
+                      </motion.div>
+                    </>
+                  ))}
+                </motion.div>
+              </motion.div>
+            ))}
+        </motion.div>
       </div>
     </>
   );
 }
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
