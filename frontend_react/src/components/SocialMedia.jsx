a2d024b9e6246b4059e1f30674f99ec1872bae93
nainoktv <nainoaktvillegas@gmail.com>
2023-01-04 18:46:38 -0800

    feat: add anchor tag for social links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -5,15 +5,25 @@ import { FaLinkedin } from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsTwitter />
-      </div>
-      <div>
-        <FaLinkedin />
-      </div>
-      <div>
-        <BsGithub />
-      </div>
+      
+      <a href="https://twitter.com/nainoa_villegas" target="_blank" rel="noreferrer">
+        <div>
+          <BsTwitter />
+        </div>
+      </a>
+
+      <a href="https://www.linkedin.com/in/nainoa-villegas/" target="_blank" rel="noreferrer">
+        <div>
+          <FaLinkedin />
+        </div>
+      </a>
+
+      <a href="https://github.com/nainoaktv" target="_blank" rel="noreferrer">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
+      
     </div>
   )
 };
