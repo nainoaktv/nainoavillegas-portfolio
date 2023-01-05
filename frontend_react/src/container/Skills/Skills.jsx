6f099881516859b334729a21ae63a2903362dbda
nainoktv <nainoaktvillegas@gmail.com>
2023-01-04 19:12:45 -0800

    feat: add animation for all sections using motionwrap

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
@@ -78,4 +78,7 @@ const Skills = () => {
   );
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'), 
+  'skills'
+);
