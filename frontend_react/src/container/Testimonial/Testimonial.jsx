cbcef5ef722498c261565a62f8b636dbaf5d3a2e
nainoktv <nainoaktvillegas@gmail.com>
2023-01-04 19:22:59 -0800

    chore: initial commit for testimonial

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Testimonial.scss';
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currenIndex, setCurrenIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+    .then((data) => {
+
+      setTestimonials(data);
+    })
+    client.fetch(brandsQuery)
+    .then((data) => {
+      setBrands(data);
+    })
+  }, [])
+
   return (
     <div>Testimonial</div>
   );
 }
 
-export default Testimonial;
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'), 
+  'testimonial'
+);
