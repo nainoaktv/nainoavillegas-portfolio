4674968d79f9a9ad49e345046d3cf73d7c1a3bb3
nainoktv <nainoaktvillegas@gmail.com>
2023-01-05 12:49:51 -0800

    feat: add testimonial to navbar and navdots, FIX contact mapping

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -4,7 +4,7 @@ const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
 
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contatct'].map((item, idx) => (
+      {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, idx) => (
        <a
          href={`#${item}`} 
          key={item + idx}
